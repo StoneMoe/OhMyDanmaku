ab33dd9c13c4300c0b76003d9123325dc1ec7a16
Stone <lakechan96@gmail.com>
2015-09-20 20:59:25 +0800

    Added a new Filter for Web Proxy

--- src/WebProxySample/proxy.php
+++ src/WebProxySample/proxy.php
@@ -1,18 +1,30 @@
 <?php
+$notAllowWords = array("fuck","shit");
+$wordFilter = true;
 
 if (isset($_POST['danmaku'])) {
+	$msg = $_POST['danmaku'];
+	//word filter
+	if ($wordFilter) {
+		foreach ($notAllowWords as $item) {
+			$msg = str_replace($item, "[Not allow]", $msg);
+		}
+	}
+	//base filter
+	$msg = str_replace("\\", "\\\\", $msg);
+	$msg = trim($msg);
+	if ($msg == "") {
+		exit();
+	}
+	//start send
 	$addr = '127.0.0.1';
 	$port = '8585';
-
 	//Create Socket
 	$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-
 	//Send to OhMyDanmaku Server
-	socket_sendto($sock, $_POST['danmaku'], strlen($_POST['danmaku']), 0, $addr, $port);
-
+	socket_sendto($sock, $msg, strlen($msg), 0, $addr, $port);
 	//CLose Socket
 	socket_close($sock);
-
 	exit();
 }
 
